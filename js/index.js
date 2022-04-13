bd44b42c7a64bdaa51f752e36a589e62eed4da71
lanbo <15701674082@163.com>
2022-04-13 16:18:19 +0800

    修改为ejs模板

--- js/index.js
+++ js/index.js
@@ -1,16 +1,17 @@
 import Vue from 'vue';
 import App from '../components/App.vue';
-import router from '../router';
-
-import { mc, MicoJSBridge, request, getPlatform, proxyUrl } from 'mico-base-common';
+import { mc, MicoJSBridge,<% if (sentry) { %> initSentry,<% } %> request, getPlatform, proxyUrl } from 'mico-base-common';
+<% if (router) { %>import router from '../router';<% } %>
 mc.install(MicoJSBridge);
 mc.install(request);
 mc.install(proxyUrl);
 mc.install(getPlatform);
+<% if (sentry) { %>mc.install(initSentry);
+mc.initSentry(Vue);<% } %>
 Vue.prototype.$mc = mc;
 
 new Vue({
-  router,
+  <% if (router) { %>router,<% } %>
   render: h => h(App)
 }).$mount('#app')
 
